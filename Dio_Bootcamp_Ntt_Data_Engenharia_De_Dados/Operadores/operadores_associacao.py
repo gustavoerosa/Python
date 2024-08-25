6c94f80eff8e8b524dee7a20d8cf1ba0acb54e93
Gustavo Rosa <gustavoe.rosa@hotmail.com>
2024-08-25 10:36:33 +0100

    add finished operators

---
+++ Dio_Bootcamp_Ntt_Data_Engenharia_De_Dados/Operadores/operadores_associacao.py
@@ -0,0 +1,15 @@
+frutas = ["limão", "uva"]
+curso = "Curso de Python"
+
+print("laranja" in frutas)
+print("limão" not in frutas)
+
+print("------------")
+
+print("laranja" not in frutas)
+print("limão" in frutas)
+
+print("------------")
+
+print("Python" in curso)
+print("python" in curso)
---
+++ Dio_Bootcamp_Ntt_Data_Engenharia_De_Dados/Operadores/operadores_identidade.py
@@ -0,0 +1,10 @@
+saldo, limite = 1000, 1000
+saldo_2, limite_2 = 1000, 500
+
+print(saldo is limite)
+print(saldo is not limite)
+
+print("-------------")
+
+print(saldo_2 is limite_2)
+print(saldo_2 is not limite_2)
