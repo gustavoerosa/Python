9125cb4b1aac7e7ab56aa2327bc27787268cc361
Gustavo Rosa <gustavoe.rosa@hotmail.com>
2024-08-26 15:19:05 +0100

    add string manipulat

---
+++ Dio_Bootcamp_Ntt_Data_Engenharia_De_Dados/manipulando_strings/string_3.py
@@ -0,0 +1,10 @@
+nome = "Gustavo Evandro Rosa"
+
+print(nome[0])
+print(nome[-2])
+print(nome[:7])
+print(nome[8:])
+print(nome[8:15])
+print(nome[8:15:2])
+print(nome[:])
+print(nome[::-1])
---
+++ Dio_Bootcamp_Ntt_Data_Engenharia_De_Dados/manipulando_strings/string_4.py
@@ -0,0 +1,28 @@
+nome = "Gustavo"
+
+mensagem_1 = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python.
+"""
+
+print(mensagem_1)
+
+mensagem_2 = f"""
+    Olá meu nome é {nome},
+Eu estou aprendendo Python.
+        Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem_2)
+
+print(
+    """
+    ----------------------    Menu    ----------------------
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    --------------------------------------------------------
+    """
+)
