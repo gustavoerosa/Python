db9070197668a33055d3b764aa32eef9240ce7b8
Gustavo Rosa <gustavoe.rosa@hotmail.com>
2024-08-27 16:55:34 +0100

    add started functions

---
+++ dio_bootcamp_ntt_data_engenharia_de_dados/05_funcoes/retorno_da_funcao.py
@@ -0,0 +1,16 @@
+def calcular_total(numeros):
+    return sum(numeros)
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1
+
+    return antecessor, sucessor
+
+def func_3():
+    print("Olá mundo!")
+    #return None
+
+print(calcular_total([10, 20, 34]))
+print(retorna_antecessor_e_sucessor(18))
+print(func_3())
