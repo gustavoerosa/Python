87de257bb50422897c8c76416903cb777e002bca
Gustavo Rosa <gustavoe.rosa@hotmail.com>
2024-08-29 16:53:47 +0100

    add started lists

---
+++ dio_bootcamp_ntt_data_engenharia_de_dados/07_listas_tuplas_dicionarios/listas/02_acesso_direto.py
@@ -0,0 +1,6 @@
+frutas = ["laranja", "maca", "uva", "pera"]
+print(frutas[0])
+print(frutas[2])
+
+print(frutas[-1])
+print(frutas[-3])
---
+++ dio_bootcamp_ntt_data_engenharia_de_dados/07_listas_tuplas_dicionarios/listas/04_fatiamento.py
@@ -0,0 +1,8 @@
+lista = ["p", "y", "t", "h", "o", "n"]
+
+lista[2:]
+lista[:2]
+lista[1:3]
+lista[0:3:2]
+lista[::]
+lista[::-1]
