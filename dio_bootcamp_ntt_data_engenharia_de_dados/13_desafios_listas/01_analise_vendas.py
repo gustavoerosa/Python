bfe760ae9c1842b4f2fde2ef075c78d2472b7116
Gustavo Rosa <gustavoe.rosa@hotmail.com>
2024-09-05 15:30:56 +0100

    add finished list challenges

---
+++ dio_bootcamp_ntt_data_engenharia_de_dados/13_desafios_listas/01_analise_vendas.py
@@ -0,0 +1,22 @@
+def analise_vendas(vendas):
+    # TODO: Calcule o total de vendas e realize a média mensal:
+    total_vendas = 0
+
+    for venda in vendas:
+        total_vendas += venda
+
+    quantidade_vendas = len(vendas)
+    media_vendas = total_vendas / quantidade_vendas
+
+    return f"{total_vendas}, {media_vendas:.2f}"
+
+def obter_entrada_vendas():
+    # Solicita a entrada do usuário em uma única linha
+    entrada = input()
+    # TODO: Converta a entrada em uma lista de inteiros:
+    vendas = list(map(int, entrada.split(",")))
+
+    return vendas
+
+vendas = obter_entrada_vendas()
+print(analise_vendas(vendas))
